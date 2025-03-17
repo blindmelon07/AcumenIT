12ed88d384d116762dbac508b73ce8f19c708aca
blindmelon07 <bryandetera07@gmail.com>
2025-03-17 10:06:44 +0800

    added execise 2 - 3

---
+++ exercise3.py
@@ -0,0 +1,21 @@
+name = input("What is your name? ")
+age = input ("What is your age? ")
+print ("Your are  " + name + " and you are " + age + " years old." )
+# What is the difference between the three snippets?
+# The difference between the three snippets is that the first snippet asks for the name and age separately and the second snippet asks for the name and age together. The third snippet asks for the name and age together and prints the name and age together.
+# Which one do you prefer and why?
+
+age = int(age) + 10 
+print ("In 10 years you will be " + str(age) + " years old." )
+# What is the output of the code?
+# The output is an error because you cannot add a string to an integer.
+# Why is this happening?
+# This is happening because you cannot add a string to an integer.
+# How can you fix it?
+# You can fix it by converting the age to an integer.
+# What is the output of the code now?
+# The output is "In 10 years you will be 10 years old."
+# What is the difference between the two snippets?
+# The difference between the two snippets is that the first snippet adds the age to the string and the second snippet adds the age to the integer.
+# Which one do you prefer and why?
+
