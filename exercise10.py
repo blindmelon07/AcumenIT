2926b2216af30085f1ebb45385c577869d14028c
blindmelon07 <bryandetera07@gmail.com>
2025-03-17 13:53:14 +0800

    quiz2

---
+++ exercise10.py
@@ -0,0 +1,21 @@
+fruits = [
+    "apple", 
+    "banana", 
+    "strawberry",
+    "kiwi", 
+    "orange"
+    ]
+
+#function sorted() returns a new sorted list
+# for fruit in sorted(fruits):
+#         print (f"I like {fruit} \n")
+        
+        
+# print(fruits)
+
+
+for fruit in reversed(sorted(fruits)):
+        print (f"I like {fruit}")
+        
+        
+# print(fruits)
---
+++ exercise11.py
@@ -0,0 +1,10 @@
+#modulo
+print(10 % 3)              #1
+print(5 % 2)               #1
+print(6 %  2)             #0
+
+print(6 % 2 == 0)         #True
+print(5 % 2 == 0)         #False
+print(10 % 3 == 0)       #False
+print(10 % 3 != 0)       #True
+print(5 % 2 != 0)        #True
